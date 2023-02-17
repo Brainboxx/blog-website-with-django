8f44295a9097282e97e3041b14e38fa3c959cb07
user <chineduudochuku66@gmail.com>
2023-02-17 20:00:21 +0100

    Initial Commit

---
+++ users/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import User
+from storages.backends.s3boto3 import S3Boto3Storage
+
+from PIL import Image
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    # def save(self, *args, **kwargs):
+    #     super().save(*args, **kwargs)
+    #
+    #     img = Image.open(self.image.path)
+    #
+    #     if img.height > 300 or img.width > 300:
+    #         output_size = (300, 300)
+    #         img.thumbnail(output_size)
+    #         img.save(self.image.path)
+
+class MediaStorage(S3Boto3Storage):
+    location = 'profile_pics'
+    file_overwrite = False
