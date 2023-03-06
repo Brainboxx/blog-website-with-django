53103d51e26fe95a23b11bb1b82a01203185f270
user <chineduudochuku66@gmail.com>
2023-03-06 20:32:38 +0100

    Initial Commit

--- blog/views.py
+++ blog/views.py
@@ -69,4 +69,4 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
 
 
 def about(request):
-    return render(request, 'blog/about.html', {'title': 'About'} )
+    return render(request, 'blog/about.html', {'title': 'About'})
