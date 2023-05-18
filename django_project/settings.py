04632cfb57255972a928fb6b6a65623587ccbcbf
user <chineduudochuku66@gmail.com>
2023-05-18 20:02:43 +0100

    Committing

--- django_project/settings.py
+++ django_project/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STORAGES = {
     "default": {"BACKEND": "storages.backends.s3boto3.S3Boto3Storage"},
