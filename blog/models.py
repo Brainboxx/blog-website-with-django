6aa2052a593723c85ee9a76e110f3309d2058ebe
user <chineduudochuku66@gmail.com>
2023-05-25 20:24:42 +0100

    Committing

--- blog/models.py
+++ blog/models.py
@@ -6,8 +6,14 @@ from django.urls import reverse
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
+    short_content = models.TextField(blank=True)
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        if not self.short_content:
+            self.short_content = self.content[:200] + '...'  # Generate shortened content
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
